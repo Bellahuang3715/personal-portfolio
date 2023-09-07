2f6f0ac9381feefdddc70e1b1dbb776df924e32d
Bella Huang <bella.huang@yourenext.ca>
2023-09-07 11:30:10 -0400

    implement video card component

---
+++ src/VideoCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Avatar } from '@mui/material'
+
+import './VideoCard.css';
+
+function VideoCard({ title, image, channel, views, timestamp, profile }) {
+  return (
+    <div className="card">
+        <img className="card-thumbnail" src={image} alt=""/>
+        <div className='card-info'>
+            <Avatar 
+                className='card-avatar'
+                alt={channel}
+                src={profile}
+            />
+            <div className='card-text'>
+                <h4>{title}</h4>
+                <p>{channel}</p>
+                <p>
+                    {views} * {timestamp}
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default VideoCard
